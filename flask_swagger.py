1403fd2f872561982d62cb04474523341cee69f8
robertlagrant <robertlagrant@gmail.com>
2018-02-21 23:43:57 +0000

    Very basic functionality - compares registered routes with the Swagger file routes

--- flask_swagger.py
+++ flask_swagger.py
@@ -7,23 +7,57 @@ class FlaskSwagger(object):
         self.app = app
         if app is not None:
             self.init_app(app, swagger_file=swagger_file)
+    
+
+    def default_http_responder(self, path, method):
+
+        def responder():
+            return self.parser.get_send_request_correct_body(path, method)
+            # return "Time to implement stuff, lazy!"
+        
+        return responder
 
 
     def init_app(self, app, swagger_file=None):
         if not swagger_file:
             raise Exception('Swagger file not specified')
         
-
+        with open(swagger_file) as f:
+            swagger_yaml = f.read()
+        
+        if not swagger_yaml:
+            raise Exception('No swagger found')
     
-        self.parser = SwaggerParser(swagger_path=swagger_file)
-        self.paths = self.parser.get_paths_data()
+        self.parser = SwaggerParser(swagger_yaml=swagger_yaml)
 
-        print(self.paths)
+        for path, path_config in self.parser.paths.items():
+            for method, method_config in path_config.items():
+                found = False
+                for rule in app.url_map.iter_rules():
+                    if rule.rule == path and method in rule.methods:
+                        found = True
+                
+                if not found:
+                    print("Warning: spec route not implemented; using example from Swagger: " + method.upper() + " " + path)
 
+                    app.add_url_rule(path, path+method, self.default_http_responder(path, method), methods=[method])
 
 
 if __name__== "__main__":
 
     app = Flask(__name__)
 
-    FlaskSwagger(app, "C:\\repos\\flask-swagger\\swagger.yaml")
+    FlaskSwagger(app, "swagger.yaml")
+
+    from waitress import serve
+
+    serve(app, host='0.0.0.0', port=5000)
+
+    # print()
+    # for rule in app.url_map.iter_rules():
+    #     print(dir(rule))
+    #     print()
+    #     print(rule, rule.rule, rule.methods)
+    
+
+    
