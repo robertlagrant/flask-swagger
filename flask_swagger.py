f2162aa3595e8b98a632bb20ae47ffa3df92b488
robertlagrant <robertlagrant@gmail.com>
2018-02-23 00:05:14 +0000

    Return example on endpoint

--- flask_swagger.py
+++ flask_swagger.py
@@ -1,4 +1,4 @@
-from flask import current_app, Flask
+from flask import current_app, Flask, jsonify
 from swagger_parser import SwaggerParser
 
 class FlaskSwagger(object):
@@ -9,11 +9,36 @@ class FlaskSwagger(object):
             self.init_app(app, swagger_file=swagger_file)
     
 
-    def default_http_responder(self, path, method):
+    # Resolves in this order:
+    # - full example on endpoint's first response
+    # - individual fields on endpoint
+    # - full example on model
+    # = individual fields on model
+    def resolve_example(self, method_config):
 
         def responder():
-            return self.parser.get_send_request_correct_body(path, method)
-            # return "Time to implement stuff, lazy!"
+            # Prep
+            if 'responses' in method_config:
+                first_response_code = sorted([key for key in method_config['responses']])[0]
+            
+            # First option
+            if 'responses' in method_config and 'examples' in method_config['responses'][first_response_code]:
+                top_response = method_config['responses'][first_response_code]['examples']['application/json']
+            
+
+
+                return jsonify(top_response)
+        
+        return responder
+    
+
+    def default_http_responder(self, path, method, method_config):
+
+        def responder():
+            print(method_config)
+            print("Finding example for " + method + " " + path)
+            #print(self.parser.get_send_request_correct_body(path, method))
+            return "Time to implement stuff, lazy!"
         
         return responder
 
@@ -40,7 +65,7 @@ class FlaskSwagger(object):
                 if not found:
                     print("Warning: spec route not implemented; using example from Swagger: " + method.upper() + " " + path)
 
-                    app.add_url_rule(path, path+method, self.default_http_responder(path, method), methods=[method])
+                    app.add_url_rule(path, path+method, self.resolve_example(method_config), methods=[method])
 
 
 if __name__== "__main__":
