587f7a70805128703c1093215cff726e8845b746
robertlagrant <robertlagrant@gmail.com>
2018-02-27 22:33:05 +0000

    ...seems...to...validate stuff?

--- flask_swagger.py
+++ flask_swagger.py
@@ -1,4 +1,4 @@
-from flask import current_app, Flask, jsonify
+from flask import current_app, Flask, jsonify, request, abort
 from swagger_parser import SwaggerParser
 
 class FlaskSwagger(object):
@@ -23,24 +23,46 @@ class FlaskSwagger(object):
             
             # First option
             if 'responses' in method_config and 'examples' in method_config['responses'][first_response_code]:
-                top_response = method_config['responses'][first_response_code]['examples']['application/json']
-            
+                example = method_config['responses'][first_response_code]['examples']['application/json']
 
-
-                return jsonify(top_response)
+                return jsonify(example)
         
         return responder
+
     
+    def before_request(self):
 
-    def default_http_responder(self, path, method, method_config):
+        print("BEFORE A REQUEST", request)
 
-        def responder():
-            print(method_config)
-            print("Finding example for " + method + " " + path)
-            #print(self.parser.get_send_request_correct_body(path, method))
-            return "Time to implement stuff, lazy!"
-        
-        return responder
+        # print('Request method', type(request.method), request.method)
+        # print('Request path', type(request.path), request.path)
+        # print('Request query string', type(request.query_string), request.query_string)
+
+        # if request.is_json:
+        #     print('Request json', type(request.get_json()), request.get_json())
+        # else:
+        #     print("Request is not JSON")
+
+        valid = self.validate_werkzeug_request(request)
+
+        if not valid:
+            abort(400)
+
+
+    def validate_werkzeug_request(self, req):
+
+        json_methods = set(['post', 'put', 'patch'])
+        method = req.method.lower()
+
+        if method in json_methods:
+            if not req.is_json:
+                raise Exception('A ' + method + ' call is not JSON. Check it has a body of well-formed JSON and the content-type is set to application/json')
+            body = req.get_json()
+        else:
+            body = None
+
+        return self.parser.validate_request(req.path, method, body=body)
+
 
 
     def init_app(self, app, swagger_file=None):
@@ -58,31 +80,35 @@ class FlaskSwagger(object):
         for path, path_config in self.parser.paths.items():
             for method, method_config in path_config.items():
                 found = False
+
+                # print(app.url_map)
                 for rule in app.url_map.iter_rules():
-                    if rule.rule == path and method in rule.methods:
+                    if rule.rule == path and method.upper() in rule.methods:
                         found = True
+                        # print(dir(rule.map.iter_rules))
                 
                 if not found:
                     print("Warning: spec route not implemented; using example from Swagger: " + method.upper() + " " + path)
 
                     app.add_url_rule(path, path+method, self.resolve_example(method_config), methods=[method])
+        
+        app.before_request(self.before_request)
 
 
 if __name__== "__main__":
 
-    app = Flask(__name__)
-
-    FlaskSwagger(app, "swagger.yaml")
-
     from waitress import serve
 
-    serve(app, host='0.0.0.0', port=5000)
+    app = Flask(__name__)
 
-    # print()
-    # for rule in app.url_map.iter_rules():
-    #     print(dir(rule))
-    #     print()
-    #     print(rule, rule.rule, rule.methods)
+    @app.route('/pet/<pet_id>')
+    def something(pet_id):
+        return "This is from code"
     
+    @app.route('/pet', methods=['POST'])
+    def something_else():
+        return 'valid'
 
-    
+    FlaskSwagger(app, "swagger.yaml")
+    # serve(app, host='0.0.0.0', port=5000)
+    app.run(debug=True, port=5000)
